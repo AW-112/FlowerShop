8790fa48681eb9d1fc7c9677f31e35b1b989ea3c
aweste <aweste@taifun-software.de>
2020-09-08 22:05:01 +0200

    Initial commit

---
+++ src/main/java/com/udacity/demoapplication/data/delivery/Delivery.java
@@ -0,0 +1,63 @@
+package com.udacity.demoapplication.data.delivery;
+
+import org.hibernate.annotations.Nationalized;
+import org.hibernate.annotations.Type;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+public class Delivery {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Nationalized
+    private String name;
+
+    @Column(name = "address_full", length = 500)
+    private String address;
+    private LocalDateTime deliveryTime;
+    @Type(type = "yes_no")
+    private Boolean completed;
+
+    public LocalDateTime getDeliveryTime() {
+        return deliveryTime;
+    }
+
+    public void setDeliveryTime(LocalDateTime deliveryTime) {
+        this.deliveryTime = deliveryTime;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
+}
